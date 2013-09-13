0379c2db73e81cebd86f024b8d97ca55f659e5e7
hamm44 <mrbighit@gmail.com>
2013-09-13 17:48:43 +1000

    Added chapter 1 notes

--- PAnalytics.R
+++ PAnalytics.R
@@ -59,12 +59,14 @@ charts.PerformanceSummary(data, colorset=rich6equal)
 ## Need a matrix with expected returns and covariances, in timeSeries format, i.e SPISECTOR
 ## http://cran.r-project.org/web/packages/fPortfolio/fPortfolio.pdf
 ## coerce zoo with as.timeSeries.foo(), as.timeSeries
+
+# Get stock data
 data2 <- Quandl(stockList, start_date=startDate, end_date=endDate, type="xts", transformation="rdiff")
 colnames(data2) <- c("AAPL", "LULU", "MX", "TGA", "NLY")
 data2 <- as.timeSeries(data2)
-data.cov <- covEstimator(data2)
-data.cov
+data.cov <- covEstimator(data2) 
 
+# define number of assets and constraints
 assets <- 5
 constraints <- c('LongOnly')   #specify as long only
 constraints <- c('minW[1:assets]=0', 'maxW[1:assets]=0.5') #specify with min weights to max weights
@@ -84,3 +86,59 @@ frontier <- portfolioFrontier(data2, spec, constraints)
 print(frontier)
 
 # plot frontier
+tailoredFrontierPlot(frontier)  #plots the efficient frontier
+
+#plot weights
+weightsPlot(frontier)
+weightsPlot(frontier, col=rainbow(assets)) # uses different colours
+weightsPlot(frontier, col=heat.colors(assets))
+
+
+
+##################### USE Quantmod Package to get symbols from YAHOO instead of Quandl#############
+### this has an easier function to search and load the data than quandl, but I prefer quandl due to it's multi
+### data source and larger datasets
+
+library("quantmod")
+
+getSymbols("AAPL") # downloads as class xts and zoo
+chartSeries(AAPL, theme="white")
+chartSeries(AAPL) #creates a bloomberg style chart
+
+ticker <- c("DJIA", "^GDAXI", "^ATX")
+data <- dailyReturn(AAPL, subset="2011-10-01:2012-09-30") # daily returns function
+
+# Creating a risk report (Excel)
+
+mean = c()
+sd = c()
+var005 = c()
+var001 = c()
+cvar005 = c()
+
+ticker = DJIA   # get all the data from Dow jones stock and it's in the time series format
+i = 1
+for (t in ticker) {
+  mean = c(mean, mean(data[,i]))
+  sd = c(sd, sd(data[,i]))
+  var005 = c(var005, quantile(data[,i], 0.05))
+  var001 = c(var001, quantile(data[,i], 0.01))
+  cvar = mean(sort(data[,i])[1:(round(length(data[,i])*0.05))])
+  i = i + 1
+}
+
+risk_report = data.frame(ticker, mean, sd, var005, var001, cvar005)
+names(risk_report) = c("Symbol", "Mean (1y)", "St. Dev (1y)", "VaR (0.05)", "VaR (0.001)", "CVaR (0.05)" )
+write.csv2(risk_report, file="risk_report.csv")
+
+risk_cor = cor(data) # correlation of the assets
+write.csv2(risk_cor, file="risk_cor.csv")
+
+
+
+
+
+
+
+
+
--- POBook.R
+++ POBook.R
@@ -1,2 +1,62 @@
 # POptimization Book
-# 
+library(fPortfolio)
+
+# preloaded data : SWX, LPP2005, SPISECTOR, GCCINDEX, SMALLCAP, MSFT
+
+# get my own data in
+startDate <-"2012-01-01"
+endDate <- "2013-09-01"
+AAPL  <- "GOOG/NASDAQ_AAPL.4"
+LULU <- "GOOG/NASDAQ_LULU.4"
+MX <- "GOOG/NYSE_MX.4"
+TGA <- "GOOG/NASDAQ_TGA.4"
+NLY <- "GOOG/NYSE_NLY.4"
+data2 <- Quandl(stockList, start_date=startDate, end_date=endDate, type="xts", transformation="rdiff")
+colnames(data2) <- c("AAPL", "LULU", "MX", "TGA", "NLY")
+data2 <- as.timeSeries(data2)
+
+end(data2)
+start(data2)
+
+# sample a time series, with or without replacement
+SAMPLE <- sample(data2[1:10],)
+SAMPLE
+class(SAMPLE)
+
+S2 <- sample(SWX[1:10,])
+S2
+class(SWX)
+
+sort(SAMPLE)
+sort(S2)
+
+data2[5:10,"MX"]
+data2["2012-01-11",]
+
+# window function to subset period
+window(data2, start = "2012-03-01", end = "2012-06-30")
+round(window(data2, start = "2012-03-01", end = "2012-06-30"), 4) 
+  #returns the results rounded to 4 decimals
+
+# Use Aggregate function create weekly, monthly, quarterly, yearly returns from daily
+# sequence of date/time stamps defining group and function to be applied
+
+# timeCalender() function returns monthly dates in vector
+# Create articial time series
+charvec <- timeCalendar()
+data3 <- matrix(round(runif(24, 0, 10)), 12)
+tS <- timeSeries(data3, charvec)
+tS
+# Create the quarterly breakpoints from the charvec seaching for the last day in a qtr for each date.
+# To suppress the double dates we make the breakpoints unique
+by <- unique(timeLastDayInQuarter(charvec))
+?timeLastDayInQuarter #computes the first/last day in given month
+by
+# create monthly sums or averages with aggregated monthly sums
+aggregate(tS, by, FUN=sum, units=c("TSQ.1", "TSQ.2"))
+
+############ chapter 2 Financial Functions to Manipulate Assets ################
+
+
+
+
