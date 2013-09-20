aab741c115f2b78576d40aad68cc2762d5a56263
hamm44 <mrbighit@gmail.com>
2013-09-20 12:10:14 +1000

    Fixed quandl search to only search ASX, NYSE, NASDAQ, LSE

--- quandlSearch.R
+++ quandlSearch.R
@@ -12,31 +12,32 @@ Quandl.auth(token)
 options(digits=4)   
 
 # user defined input variables
-stocks <- c("AAPL", "WNC", "EBIX", "MX", "TGA", "NLY") # list the codes you want to search, and find codes for
-startDate <-"2012-01-01"
-endDate <- "2013-09-01" 
-trans <- "rdiff" #diff, cumul, normalize
+# stocks <- c("AAPL", "WNC", "EBIX", "MX", "TGA", "NLY") # list the codes you want to search, and find codes for
+# startDate <-"2012-01-01"
+# endDate <- "2013-09-01" 
+# trans <- "rdiff" #diff, cumul, normalize
 # collapse <- "none" #daily, weekly, monthly, quarterly, annual
 
 # initialise list, then find the codes using regexp on the search results
-stockD <- function(stocks, startDate, endDate, trans){
+stockCodes <- function(stocks){
   code <- list()
   for (i in stocks) {
-    res <- as.character(Quandl.search(i, source="GOOG"))
-    resInd <- regexpr(paste("GOOG/[A-Z]*_", i, sep=""), res) # searches with google data
+    res <- as.character(Quandl.search(i, source="GOOG", silent=TRUE))
+    resInd <- regexpr(paste("GOOG/[A|N][A-Z]+_", i, sep=""), res) # searches with google data
     code[[i]] <- (regmatches(res, resInd))
   } 
   
   # Make the list into a vector and add .4 ( to return the 4th column of data)
-  stockList <- paste(unlist(code), ".4", sep="")
+  stockL <- paste(unlist(code), ".4", sep="")
+  return(stockL)
+}
+
   # once we have the codes, we want to download the data
-  data <- Quandl(stockList, start_date=startDate, end_date=endDate, type="zoo", transformation=trans)
-  names(data) <- stocks
+stockData <- function(stockC, startDate, endDate, trans){  
+  data <- Quandl(stockC, start_date=startDate, end_date=endDate, type="zoo", transformation=trans)
   
   # show the data format
-  head(data)
   return(data)
 }
 
 
-# test for dow jones
